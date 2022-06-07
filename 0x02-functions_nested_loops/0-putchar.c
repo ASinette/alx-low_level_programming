16befd9fbe6bd29ea073d7d6e7540297efbeba28
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-06-07 13:58:44 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-int _putchar(char c);
+#include "main.h"
+#include <string.h>
 
-void print_alphabet(void);
+/**
+* main - Prints numbers between 00 to 89.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i = 0;
 
-void print_alphabet_x10(void);
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 
-int _islower(int c);
+	_putchar('\n');
 
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+	return (0);
+}
