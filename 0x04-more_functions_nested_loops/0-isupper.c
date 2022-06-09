3dba143c77fdd3be7d593042a1bbd9b001d2e5b1
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-06-09 15:03:59 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,21 @@
-git add .
-git commit -m "Programming in c today"
-git push
+
+#include "main.h"
+/**
+ * _isupper-evaluate if letter is uppercase
+ *@c: print int
+ * Return: 0 if successful
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
