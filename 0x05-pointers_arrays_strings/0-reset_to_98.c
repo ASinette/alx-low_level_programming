06d97792bdd608551b7df0aeb689dfed4395b829
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-06-13 15:34:28 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,10 @@
-0-reset_to_98.c
+#include "main.h"
+/**
+ * reset_to_98 - update and interger value to 98
+ * @n: A pointer to an int that will be updated
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
