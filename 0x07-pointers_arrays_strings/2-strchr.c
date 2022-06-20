65cb89ca8b20bb944b9c17aef298e1973a516bfe
ASinette <precieuseakoutey@gmail.com>
2022-06-20 07:14:19 -0700

    Programming in c today

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,37 +1,35 @@
 #include "main.h"
 
+
+
 /**
- * _strchr - locates a character in a string
- * @s: string input
- * @c: character to find
- * Return: pointer to first occurence of c character
- */
+ *
+ *  * _strchr - locates a character in a string
+ *
+ *   * @s: string input
+ *
+ *    * @c: character to find
+ *
+ *     * Return: pointer to first occurence of c character
+ *
+ *      */
+
+
 
 char *_strchr(char *s, char c)
-<<<<<<< HEAD
-	
-=======
->>>>>>> 224b8e57151eb2c95888167e5db715df2accffc4
+
 {
-unsigned int i;
 
-<<<<<<< HEAD
-unsigned int i;
+	unsigned int i;
 
 
 
-for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 
-if (s[i] == c)
+		if (s[i] == c)
 
-break;
+			break;
 
-return (s[i] == c ? (s + i) : '\0');
+	return (s[i] == c ? (s + i) : '\0');
 
-=======
-for (i = 0; s[i] != '\0'; i++)
-if (s[i] == c)
-break;
-return (s[i] == c ? (s + i) : '\0');
->>>>>>> 224b8e57151eb2c95888167e5db715df2accffc4
 }
